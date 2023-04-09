0af3cebc2ebf5776ce0d467a102c4b16b1847a47
ManausK <1905246@kiit.ac.in>
2023-04-09 18:17:35 +0530

    Storing data using useState

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,40 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AllStyle.css";
 
 function Task() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    remember: "",
+  });
+
+  const onChangeHandler = (event) => {
+    console.log(event);
+
+    setFormData(() => ({
+      ...formData,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
     <div className="Container">
       <div className="left">
         <div className="dono">
           <div className="asity"></div>
+
           <div className="form">
             <div className="Formbox">
               <div className="first">
                 <span>Welcome back, Manas</span>
-                <text>Welcome back! Please enter your details</text>
+                <div className="text">
+                  Welcome back! Please enter your details
+                </div>
               </div>
               <div className="btn">
                 <div className="google"></div>
                 <span>Log in with Google</span>
               </div>
               <div className="inputBox">
-                <input type="email" required="required" />
+                <input
+                  type="email"
+                  required="required"
+                  name="email"
+                  onChange={onChangeHandler}
+                />
                 <span>Email</span>
                 <i></i>
               </div>
               <div className="inputBox">
-                <input type="password" required="required" />
+                <input
+                  type="password"
+                  required="required"
+                  name="password"
+                  onChange={onChangeHandler}
+                />
                 <span>Password</span>
                 <i></i>
               </div>
               <div className="content">
                 <div className="checkbox">
-                  <input type="checkbox" id="remember-me" />
-                  <label for="remember-me">Remember for 30 days</label>
+                  <input
+                    type="checkbox"
+                    id="remember-me"
+                    name="remember"
+                    onChange={onChangeHandler}
+                  />
+                  <label htmlFor="remember-me">Remember for 30 days</label>
                 </div>
                 <a href="#">Forgot Password</a>
               </div>
-              <input type="submit" value="Log in" />
+              <button
+                className="button"
+                type="button"
+                value="Log in"
+                onClick={() => console.log(formData)}
+              >
+                Log in
+              </button>
               <div className="last">
                 <span>Don't have an account?</span>
                 <a href="#">Sign up for free</a>
@@ -47,5 +87,4 @@ function Task() {
     </div>
   );
 }
-
 export default Task;
